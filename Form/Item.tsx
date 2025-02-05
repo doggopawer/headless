a8255c347e79ace839e80d43fd09a6c0282b7767
dhkim <dhkim@mleverage.co.kr>
2025-02-05 16:57:30 +0900

    feat: Form 컴포넌트 생성 - Item 하위 컴포넌트 생성

---
+++ Form/Item.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useForm } from './Form';
+
+type ItemProps = {
+    children: React.ReactElement;
+    name: string;
+};
+
+const Item = ({ children, name }: ItemProps) => {
+    const { formValue, changeFormValue } = useForm();
+
+    const handleItemChange = (name: string) => (value: string) => {
+        changeFormValue(name, value);
+    };
+
+    useEffect(() => {
+        console.log('formValue', formValue);
+    }, [formValue]);
+
+    const childWithProps = React.cloneElement(
+        children as React.ReactElement<{ onFormChange?: (value: string) => void }>,
+        {
+            onFormChange: handleItemChange(name),
+        }
+    );
+
+    return <div>{childWithProps}</div>;
+};
+
+export default Item;
