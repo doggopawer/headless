09366d362ea592f24fa9f4428da937ed139ee13c
dhkim <dhkim@mleverage.co.kr>
2025-02-05 10:19:28 +0900

    feat: Modal.Content 컴포넌트 수정 - Content 외부 영역 클릭시 닫히는 기능 추가

--- Modal/Content.tsx
+++ Modal/Content.tsx
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { useModal } from './Modal';
 import { css, SerializedStyles } from '@emotion/react';
 
@@ -14,13 +14,42 @@ const contentStyle = css`
 
 type ContentProps = {
     children: React.ReactNode;
-    defaultStyle?: SerializedStyles; // css를 선택적 속성으로 설정
+    defaultStyle?: SerializedStyles; // 선택적 css 속성
 };
 
 const Content = ({ children, defaultStyle }: ContentProps) => {
-    const { modalValue } = useModal();
+    const { modalValue, closeModal } = useModal();
+    const containerRef = useRef<HTMLDivElement>(null);
 
-    return <>{modalValue && <div css={[contentStyle, defaultStyle]}>{children}</div>}</>;
+    useEffect(() => {
+        // 외부 클릭 감지 핸들러
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                containerRef.current && // ref가 존재하고
+                !containerRef.current.contains(event.target as Node) // 클릭 대상이 모달 내부가 아니라면
+            ) {
+                closeModal();
+            }
+        };
+
+        if (modalValue) {
+            document.addEventListener('mousedown', handleClickOutside);
+        }
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [modalValue, closeModal]);
+
+    return (
+        <>
+            {modalValue && (
+                <div ref={containerRef} css={[contentStyle, defaultStyle]}>
+                    {children}
+                </div>
+            )}
+        </>
+    );
 };
 
 export default Content;
