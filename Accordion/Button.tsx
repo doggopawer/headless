1efd518c92141a20f71c9c340e729ce2cacd69d2
mlevearge-dhkim <dhkim@mleverage.co.kr>
2025-02-15 23:28:52 +0900

    feat: AccordionButton 수정 - onButtonClick에서 반환값 받도록 수정

--- Accordion/Button.tsx
+++ Accordion/Button.tsx
@@ -1,21 +1,25 @@
 /** @jsxImportSource @emotion/react */
-import { SerializedStyles } from '@emotion/react';
-import React, { useEffect } from 'react';
-import { useAccordion } from './Accordion';
+import {SerializedStyles} from "@emotion/react";
+import React, {useEffect} from "react";
+import {useAccordion} from "./Accordion";
 
 type ButtonProps = {
     children?: React.ReactNode;
-    onButtonClick?: (value: boolean) => void;
+    onButtonClick?: (value: boolean) => void | boolean;
     defaultStyle?: SerializedStyles; // css를 선택적 속성으로 설정
 };
 
-const Button = ({ children, defaultStyle }: ButtonProps) => {
-    const { accordionValue, toggleAccordion } = useAccordion();
+const Button = ({children, defaultStyle, onButtonClick}: ButtonProps) => {
+    const {accordionValue, toggleAccordion} = useAccordion();
     const handleButtonClick = () => {
+        const result = onButtonClick && onButtonClick(accordionValue);
+        if (result) {
+            return;
+        }
         toggleAccordion();
     };
     useEffect(() => {
-        console.log('accordionValue', accordionValue);
+        console.log("accordionValue", accordionValue);
     }, [accordionValue]);
     return (
         <button css={[defaultStyle]} onClick={handleButtonClick}>
