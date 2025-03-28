4760855f38c369c5cd5198b1a2e4bfa2ed27b655
dhkim <dhkim@mleverage.co.kr>
2025-03-28 10:17:50 +0900

    feat: Dropdown.Content 수정 - 화면 끝에 있을 경우 자동 위치 보정 기능 추가

--- Dropdown/Content.tsx
+++ Dropdown/Content.tsx
@@ -1,7 +1,5 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState, useLayoutEffect } from 'react';
 import { useDropdown } from './Dropdown';
-import classNames from 'classnames';
-import styles from './Dropdown.module.scss';
 
 type ContentProps = React.HTMLAttributes<HTMLDivElement> & {
     children: React.ReactNode;
@@ -10,8 +8,9 @@ type ContentProps = React.HTMLAttributes<HTMLDivElement> & {
 const Content = ({ children, ...props }: ContentProps) => {
     const { dropdownValue, closeDropdown } = useDropdown();
     const containerRef = useRef<HTMLDivElement>(null);
+    const [offset, setOffset] = useState(0);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         const handleClickOutside = (event: MouseEvent) => {
             if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
                 closeDropdown();
@@ -20,25 +19,39 @@ const Content = ({ children, ...props }: ContentProps) => {
 
         if (dropdownValue) {
             document.addEventListener('mousedown', handleClickOutside);
+            // 위치 보정 로직
+            const rect = containerRef.current?.getBoundingClientRect();
+            if (rect) {
+                const overflow = rect.right - window.innerWidth;
+                console.log(rect.right, window.innerWidth);
+                console.log('overflow: ' + overflow);
+                if (overflow > 0) {
+                    setOffset(rect.width);
+                } else {
+                    setOffset(0);
+                }
+            }
+        } else {
+            setOffset(0);
         }
+
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
     }, [dropdownValue, closeDropdown]);
 
-    const combinedStyle = classNames(
-        props.className, // 외부에서 전달받은 클래스
-        styles.Content,
-        {
-            [styles.Open]: dropdownValue, // dropdownValue가 true일 때 Open 클래스 적용
-            [styles.Closed]: !dropdownValue, // dropdownValue가 false일 때 Closed 클래스 적용
-        }
-    );
-
     return (
-        <div ref={containerRef} {...props} className={combinedStyle}>
-            {children}
-        </div>
+        <>
+            {dropdownValue && (
+                <div
+                    ref={containerRef}
+                    {...props}
+                    style={{ transform: offset ? `translateX(-${offset}px)` : undefined }}
+                >
+                    {children}
+                </div>
+            )}
+        </>
     );
 };
 
